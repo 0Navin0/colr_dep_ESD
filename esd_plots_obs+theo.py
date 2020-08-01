6345bdbbed0e94d12753d584b175a05022f8fb0e
navin <navin@gmail.com>
2020-08-02 01:56:18 +0530

    plotted and saved ratio plots of esd signals...singlePlots and overplots both.
    In esd signal plots(not 'ratio' ones) only 'nan' data (deltasigma) points were removed while plotting.It still contains negative 'deltasigma' values from observed files(outputted from weaklensing pipeline.)
    But while plotting ratio plots, I removed  both 'nan' and 'negative' 'deltasigma' points. And only these points are plotted by ESD from AUM (However, I still use ESD to calculate 'w.l. signal' at those radii points too, which observationally gives(through weaklensing pipeline--Surhud M.) 'nan' or 'negative' values.).

--- esd_plots_obs+theo.py
+++ esd_plots_obs+theo.py
@@ -5,9 +5,9 @@ from glob import glob
 import numpy as np
 from astropy.io import fits
 import json
-#from subprocess import call
+from subprocess import call
 
-def plot_esd(method):
+def plot_esd(method, single):
     with open(f'./esd_{method}_magbinned_colrdep.json','r') as f: 
         dic = json.load(f)
     magbin = np.array(list(dic.keys()))
@@ -21,8 +21,11 @@ def plot_esd(method):
     for tag in identifier:
         #fig,axes = plt.subplots(nrows=2,ncols=2)
         #for ii,colr in zip(range(len(axes)),colour): #accessing one row at a time.
+        
+        if single==False:
+            #for both colr on the same plot 
+            fig,axes = plt.subplots(1,1)
 
-        fig,axes = plt.subplots(1,1)
         for colr in colour:
             # get data from weaklensing pipeline output to plot
             deltasigma,r,errDeltaSigma = np.loadtxt('%s%d_%s.dat'%(base,tag,colr),usecols=(5,7,12),unpack=True)
@@ -36,49 +39,121 @@ def plot_esd(method):
             leg1 = "abs_mag_bin:(%s,%s)"%(hdr['absmmin'][-8:-3],hdr['absmmax'][-8:-3])
             leg2 = "z-bin: %s" %hdr['z_bin'].split('-')[0][:4]+'-'+(hdr['z_bin'].split('-')[1][:4])
             leg3 = '%s galaxy sample'%colr
+            print("Infromation of observed signal from weaklens pipeline(Credit:Surhud M.):")
+            print(f"{leg1}\nNo of nan values in \"{colr}\" esd/deltasigma,r,errDeltaSigma: {np.sum(np.isnan(deltasigma))},{np.sum(np.isnan(r))},{np.sum(np.isnan(errDeltaSigma))}")
+            print(f"No of negative points in\"{colr}\" esd/deltasigma,errDeltaSigma: {np.sum(deltasigma<0)},{np.sum(errDeltaSigma<0)}\n\n" )
+
             #get calculated(theoretical) ESD from AUM
             if colr=='red':
                 esd = dic[magbin[1:][binmax==float(hdr['absmmax'])][0]][0]['red']
             else: 
                 esd = dic[magbin[1:][binmax==float(hdr['absmmax'])][0]][1]['blue']
-         
-        #    # when plotting one colr on one plot
-        #    fig,axes = plt.subplots(1,1)
-        #    axes.plot(rp, esd, label=f"prediction(by AUM): Niladri et al.",c=colr) #marker='d', markerfacecolor='white',
-        #    axes.errorbar(r, deltasigma, yerr=errDeltaSigma,c=colr, marker='o', label='Observed: HSC_data', fmt='o', linewidth=1.5, capsize=5, capthick=2)
-        #    axes.errorbar([],[],markerfacecolor='None',ls='',label=f"{leg1}\n{leg2}")
-        #    #axes.scatter([],[],facecolors='None',label="=======")
-        #    axes.set_xscale('log')
-        #    axes.set_yscale('log')    
-        #    axes.legend(loc='best', frameon=True)
-        #    # setting common x and y axes lables.
-        #    fig.text(0.5, 0.02, r"$R [h^{-1} Mpc$]", ha='center')#, fontsize=16)
-        #    fig.text(0.04, 0.5, '$\Delta\Sigma$ [hM$_\odot$/pc$^2]$', va='center', rotation='vertical')#, fontsize=16)
-        #    # for common title to all the subplots
-        #    plt.savefig(f"{colr}_{hdr['absmmin'][-8:-3],hdr['absmmax'][-8:-3]}.png")
-    
-            # plotting red and blue on the same plot (comment above block and uncomment the axes outside this loop.) 
-            if colr=='blue':
-                axes.plot(rp, esd, label=f"prediction(by AUM): Niladri et al.",c=colr) #marker='d', markerfacecolor='white'
+            if single==True:
+                # plotting one colr on one plot
+                fig,axes = plt.subplots(1,1)
+                axes.plot(rp, esd, label=f"Prediction:(model HOD)Niladri et al.",c=colr) #marker='d', markerfacecolor='white',
                 axes.errorbar(r, deltasigma, yerr=errDeltaSigma,c=colr, marker='o', label='Observed: HSC_data', fmt='o', linewidth=1.5, capsize=5, capthick=2)
+                axes.errorbar([],[],markerfacecolor='None',ls='',label=f"{leg1}\n{leg2}")
+                #axes.scatter([],[],facecolors='None',label="=======")
+                axes.set_xscale('log')
+                axes.set_yscale('log')    
+                axes.legend(loc='best', frameon=True)
+                # setting common x and y axes lables.
+                fig.text(0.5, 0.02, r"$R [h^{-1} Mpc$]", ha='center')#, fontsize=16)
+                fig.text(0.04, 0.5, '$\Delta\Sigma$ [hM$_\odot$/pc$^2]$', va='center', rotation='vertical')#, fontsize=16)
+                # for common title to all the subplots
+                plt.savefig(f"{colr}_{hdr['absmmin'][-8:-3],hdr['absmmax'][-8:-3]}.png")
             else:
-                axes.plot(rp, esd, c=colr) #, marker='d',markerfacecolor='white'
-                axes.errorbar(r, deltasigma, yerr=errDeltaSigma,c=colr, marker='o', fmt='o', linewidth=1.5, capsize=5, capthick=2)
-        axes.errorbar([],[],markerfacecolor='None',ls='',label=f"{leg1}\n{leg2}")
-        axes.set_xscale('log')
-        axes.set_yscale('log')    
-        axes.legend(loc='best', frameon=True)
-        # setting common x and y axes lables.
-        fig.text(0.5, 0.02, r"$R [h^{-1} Mpc$]", ha='center')#, fontsize=16)
-        fig.text(0.04, 0.5, '$\Delta\Sigma$ [hM$_\odot$/pc$^2]$', va='center', rotation='vertical')#, fontsize=16)
-        # for common title to all the subplots
-        #plt.suptitle(r'samples used for weaklensing singal calculation (NYU_VAGC,Zehavi,Niladri)')
-        #plt.suptitle('weak lensing signal around NYU-VAGC galaxy sample dr72safe%d in HSC field of sources.\n\n%s, %s'%(tag,leg1,leg2))#, fontsize=15)
-        #plt.show()
-        plt.savefig(f"{hdr['absmmin'][-8:-3],hdr['absmmax'][-8:-3]}.png")
+                # plotting red and blue on the same plot 
+                if colr=='blue':
+                    axes.plot(rp, esd, label=f"Prediction:(HOD model)Niladri et al.",c=colr) #marker='d', markerfacecolor='white'
+                    axes.errorbar(r, deltasigma, yerr=errDeltaSigma,c=colr, marker='o', label='Observed: HSC_data', fmt='o', linewidth=1.5, capsize=5, capthick=2)
+                else:
+                    axes.plot(rp, esd, c=colr) #, marker='d',markerfacecolor='white'
+                    axes.errorbar(r, deltasigma, yerr=errDeltaSigma,c=colr, marker='o', fmt='o', linewidth=1.5, capsize=5, capthick=2)
+        if single==False:
+            axes.errorbar([],[],markerfacecolor='None',ls='',label=f"{leg1}\n{leg2}")
+            axes.set_xscale('log')
+            axes.set_yscale('log')    
+            axes.legend(loc='best', frameon=True)
+            # setting common x and y axes lables.
+            fig.text(0.5, 0.02, r"$R [h^{-1} Mpc$]", ha='center')#, fontsize=16)
+            fig.text(0.04, 0.5, '$\Delta\Sigma$ [hM$_\odot$/pc$^2]$', va='center', rotation='vertical')#, fontsize=16)
+            # for common title to all the subplots
+            #plt.suptitle(r'samples used for weaklensing signal calculation (NYU_VAGC,Zehavi,Niladri)')
+            #plt.suptitle('weak lensing signal around NYU-VAGC galaxy sample dr72safe%d in HSC field of sources.\n\n%s, %s'%(tag,leg1,leg2))#, fontsize=15)
+            #plt.show()
+            plt.savefig(f"{hdr['absmmin'][-8:-3],hdr['absmmax'][-8:-3]}.png")
    
 if __name__=="__main__":
-    #plot_esd(method='bestfit') 
-    plot_esd(method='fittingFunc')
-  
+    method = ['bestfit','fittingFunc']
+    for ii in method:
+        for jj,tt in zip((True,False),("single","overplot")):
+            plot_esd(method=ii,single=jj) 
+            cmd=f"mkdir ./{tt}.{ii}"
+            call(cmd,shell=True)
+            call(f"mv *png ./{tt}.{ii}",shell=True)
+            call(f"tar -czvf ./{tt}.{ii}.tar.gz ./{tt}.{ii}",shell=True)
+    call("mkdir -p esd_plots",shell=True)
+    call("mv single* overplot* ./esd_plots",shell=True)
 
+
+
+"""
+
+At some stage I check in the files outputted from weaklens pipeline:
+1)There are some negative values in the esd/deltasigma in the observed signal(which was calulated from weaklens pipeline.)
+Do you understan why would esd be negative theoretically?
+2) Also there are nan values in deltasigma and errDeltaSigma.
+-------> nan values are filtered out but negative values are not yet removed...
+------->-ve values doesn't cause any problem in plotting since we are not taking log()of these numbers...only scale is in log.
+------->But in ratio plots these -ve points are excluded.
+
+Infromation of observed signal from weaklens pipeline(Credit:Surhud M.):
+Notice: nan values are absent only because the prints stements are taken after filter nan entries.
+abs_mag_bin:(-23.0,-22.0)
+No of nan values in "red" esd/deltasigma,r,errDeltaSigma: 0,0,0
+No of negative points in"red" esd/deltasigma,errDeltaSigma: 0,0
+
+
+Infromation of observed signal from weaklens pipeline(Credit:Surhud M.):
+abs_mag_bin:(-23.0,-22.0)
+No of nan values in "blue" esd/deltasigma,r,errDeltaSigma: 0,0,0
+No of negative points in"blue" esd/deltasigma,errDeltaSigma: 0,0
+
+
+Infromation of observed signal from weaklens pipeline(Credit:Surhud M.):
+abs_mag_bin:(-22.0,-21.0)
+No of nan values in "red" esd/deltasigma,r,errDeltaSigma: 0,0,0
+No of negative points in"red" esd/deltasigma,errDeltaSigma: 0,0
+
+
+Infromation of observed signal from weaklens pipeline(Credit:Surhud M.):
+abs_mag_bin:(-22.0,-21.0)
+No of nan values in "blue" esd/deltasigma,r,errDeltaSigma: 0,0,0
+No of negative points in"blue" esd/deltasigma,errDeltaSigma: 0,0
+
+
+Infromation of observed signal from weaklens pipeline(Credit:Surhud M.):
+abs_mag_bin:(-21.0,-20.0)
+No of nan values in "red" esd/deltasigma,r,errDeltaSigma: 0,0,0
+No of negative points in"red" esd/deltasigma,errDeltaSigma: 0,0
+
+
+Infromation of observed signal from weaklens pipeline(Credit:Surhud M.):
+abs_mag_bin:(-21.0,-20.0)
+No of nan values in "blue" esd/deltasigma,r,errDeltaSigma: 0,0,0
+No of negative points in"blue" esd/deltasigma,errDeltaSigma: 7,0
+
+
+Infromation of observed signal from weaklens pipeline(Credit:Surhud M.):
+abs_mag_bin:(-20.0,-19.0)
+No of nan values in "red" esd/deltasigma,r,errDeltaSigma: 0,0,0
+No of negative points in"red" esd/deltasigma,errDeltaSigma: 3,0
+
+
+Infromation of observed signal from weaklens pipeline(Credit:Surhud M.):
+abs_mag_bin:(-20.0,-19.0)
+No of nan values in "blue" esd/deltasigma,r,errDeltaSigma: 0,0,0
+No of negative points in"blue" esd/deltasigma,errDeltaSigma: 6,0
+"""
---
+++ esd_ratioPlot_bestfit.py
@@ -0,0 +1,115 @@
+# a modification of code in "git/colr_dep_ESD/esd_plots_obs+theo.py"
+
+import matplotlib.pyplot as plt 
+from glob import glob
+import numpy as np
+from astropy.io import fits
+import json
+from subprocess import call
+
+def ratioplot_esd(method, single):
+    with open(f'./esd_{method}_magbinned_colrdep.json','r') as f: 
+        dic = json.load(f)
+    magbin = np.array(list(dic.keys()))
+    binmax = np.array([float(magbin[ii+1][:5]) for ii in range(len(magbin)-1)])
+    rp = dic['rp']
+ 
+    base = '/home/navin/Tractwise_data/nyu-vagc/iditSmaples/result_weaklen_pipeline/signal_dr72safe'
+    sample_base = '/home/navin/Tractwise_data/nyu-vagc/iditSmaples/col_dep_filter_fits/post_catalog.dr72safe'
+    identifier = [7,8,9,10]
+    colour = ['red','blue']
+    for tag in identifier:
+        if single==False:
+            #for both colr on the same plot 
+            fig,axes = plt.subplots(1,1)
+
+        for colr in colour:
+
+            # get info of samples to be plotted from the red/blue filtered fits files
+            hdul = fits.open('%s%d.%s.fits'%(sample_base,tag,colr))
+            hdr = hdul[0].header
+            leg1 = "abs_mag_bin:(%s,%s)"%(hdr['absmmin'][-8:-3],hdr['absmmax'][-8:-3])
+            leg2 = "z-bin: (%s)" %(hdr['z_bin'].split('-')[0][:4]+'-'+(hdr['z_bin'].split('-')[1][:4]))
+            leg3 = '%s galaxy sample'%colr
+
+            # get data from weaklensing pipeline output to plot
+            deltasigma,r,errDeltaSigma = np.loadtxt('%s%d_%s.dat'%(base,tag,colr),usecols=(5,7,12),unpack=True) 
+
+            #to apply filter to rp to get only those radii which are valid points in observed signal files of each color.
+            rp_ = np.array(rp)
+
+            #debugging step
+            print(f"rp_.size={rp_.size},r.size={r.size}: should be like (15,<=15).")
+            print(f"{r==rp_}: if True apply same filter on 'rp' values as on 'r'.")
+            print(f"{leg1}\nNo of nan values in \"{colr}\" esd/deltasigma,r,errDeltaSigma: {np.sum(np.isnan(deltasigma))},{np.sum(np.isnan(r))},{np.sum(np.isnan(errDeltaSigma))}")
+            print(f"No of negative points in \"{colr}\" esd/deltasigma,errDeltaSigma: {np.sum(deltasigma<0)},{np.sum(errDeltaSigma<0)}" ) #\n\n
+
+            notnan = ~np.isnan(deltasigma) & ~np.isnan(r) & ~np.isnan(errDeltaSigma)
+            positives = ~(deltasigma<0) & ~(errDeltaSigma<0)
+            deltasigma=deltasigma[notnan & positives]
+            r = r[notnan & positives]
+            errDeltaSigma = errDeltaSigma[notnan & positives]
+
+            #get calculated(theoretical) ESD from AUM
+            if colr=='red':
+                esd = dic[magbin[1:][binmax==float(hdr['absmmax'])][0]][0]['red']
+            else: 
+                esd = dic[magbin[1:][binmax==float(hdr['absmmax'])][0]][1]['blue']
+
+            #to get the ratio plot
+            rp_ = rp_[notnan & positives] #get same points of radii at which 'deltasigma' is available. 
+            esd = np.array(esd)[notnan & positives]
+            #debugging step
+            print(f"predicted esd size={esd.size}\tobserved deltasigma size{deltasigma.size}\tr(weaklens output files) size={r.size}\trp(ESD_aum)={rp_.size}\n\n")
+            ratio = deltasigma/esd #obervational/theoretical
+
+            if single==True:
+                # plotting one colr on one plot
+                fig,axes = plt.subplots(1,1)
+                axes.plot(rp_, ratio, label=r"$\frac{Observed(NYU-VAGC\ cat.+Pipeline)}{Prediction(Niladri\ et\ al.+AUM)}$",c=colr) #marker='d', markerfacecolor='white',
+                axes.errorbar([],[],markerfacecolor='None',ls='',label=f"{leg1}\n{leg2}")
+                axes.set_xscale('log')
+                #axes.set_yscale('log')    
+                axes.legend(loc='best', frameon=True)
+                # setting common x and y axes lables.
+                fig.text(0.5, 0.02, r"$R [h^{-1} Mpc$]", ha='center')#, fontsize=16)
+                #fig.text(0.04, 0.5, r'$\frac{Predicted(\Delta\Sigma)}{Observed(\Delta\Sigma)}$', va='center', rotation='vertical')#, fontsize=16) #units_deltasigma=[hM$_\odot$/pc$^2]
+              
+                # for common title to all the subplots
+                plt.savefig(f"{colr}_{hdr['absmmin'][-8:-3],hdr['absmmax'][-8:-3]}.png")
+            
+            if single==False:
+                axes.plot(rp_, ratio,c=colr) #marker='d', markerfacecolor='white'
+        if single==False:
+            axes.errorbar([],[],markerfacecolor='None',ls='',label=f"{leg1}\n{leg2}")
+            axes.errorbar([],[],markerfacecolor='None',ls='',label=r"$\frac{Observed(NYU-VAGC\ cat.+Pipeline)}{Prediction(Niladri\ et\ al.+AUM)}$")
+            axes.set_xscale('log')
+            #axes.set_yscale('log')    
+            axes.legend(loc='best', frameon=True)
+            # setting common x and y axes lables.
+            fig.text(0.5, 0.02, r"$R [h^{-1} Mpc$]", ha='center')#, fontsize=16)
+            #fig.text(0.04, 0.5, r'$\frac{Predicted(\Delta\Sigma)}{Observed(\Delta\Sigma)}$', va='center', rotation='vertical')#, fontsize=16)
+
+            ## for common title to all the subplots
+            #plt.suptitle(r'samples used for weaklensing single calculation (NYU_VAGC,Zehavi,Niladri)')
+            #plt.suptitle('weak lensing signal around NYU-VAGC galaxy sample dr72safe%d in HSC field of sources.\n\n%s, %s'%(tag,leg1,leg2))#, fontsize=15)
+
+            plt.savefig(f"{hdr['absmmin'][-8:-3],hdr['absmmax'][-8:-3]}.png")
+
+if __name__=="__main__":
+    method = ['bestfit','fittingFunc']
+    for ii in method:
+        for jj,tt in zip((True,False),("single","overplot")):
+            ratioplot_esd(method=ii,single=jj) 
+            cmd=f"mkdir -p ./{tt}.{ii}.ratio"
+            call(cmd,shell=True)
+            call(f"mv *png ./{tt}.{ii}.ratio",shell=True)
+            call(f"tar -cvzf ./{tt}.{ii}.ratio.tar.gz ./{tt}.{ii}.ratio",shell=True)
+    call("mkdir -p esd_ratio_plots",shell=True)
+    call("mv *ratio *ratio.tar.gz ./esd_ratio_plots",shell=True)
+"""
+understand units of deltasigma theoretically as well as the way it gets caclulated in ESd and weaklens pipeline(observational).
+
+removed 'nan' and 'negative' 'deltasigma' points from 'deltasigma' of obserevd weaklensing pipeline outputs. 
+correspondingly the 'esd' points from ESD(using AUM and HOD) with same radii are not plotted. 
+"""
